0993fcc7013a7b675720c5aff69d7b87a1475470
Kenji Uno <Kenji Uno>
2017-09-18 11:08:58 +0900

    Add project files.

---
+++ OcrTest2/OcrTest2/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace OcrTest2 {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new OcrTest2.MainPage();
+        }
+
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
